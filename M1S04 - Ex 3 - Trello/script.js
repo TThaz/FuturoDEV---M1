7499cf4492a5c7e4dec8fe821be98abd50704dcb
Thalles Azevedo <contato.thalles@outlook.com>
2024-02-13 11:13:04 -0300

    Exercícios 1, 2, 3 e 4 adicionados
    Exercícios referente ao curso FuturoDEV

---
+++ M1S04 - Ex 3 - Trello/script.js
@@ -0,0 +1,5 @@
+// Combine duas strings usando o operador de concatenação e exiba o resultado no console.
+
+// Utilize o template string ou a concatenação simples.
+
+console.log("Que dia legal " + "para programar!")
