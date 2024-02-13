7872efe1b1568bfd37973b5136891696558bc0f4
Thalles Azevedo <contato.thalles@outlook.com>
2024-02-13 11:47:34 -0300

    Exercício 6
    Resolução do exercício 6

---
+++ M1S04 - Ex 6 - Trello/script.js
@@ -0,0 +1,10 @@
+// // Crie 3 objetos com propriedades representando informações pessoais 
+// (nome, idade, nacionalidade e profissão) 
+// e exiba essas informações com um console.log para cada objeto.
+
+let nome = document.querySelector("#nome").textContent;
+let idade = document.querySelector("#idade").textContent;
+let nacionalidade = document.querySelector("#nacionalidade").textContent;
+let profissao = document.querySelector("#profissao").textContent;
+
+console.log(`${nome} é um rapaz de ${idade} anos, ${nacionalidade} e ${profissao}`)
