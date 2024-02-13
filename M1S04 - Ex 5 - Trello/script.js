06e4de43b84adb83199bc22b6161fd29c5223877
Thalles Azevedo <contato.thalles@outlook.com>
2024-02-13 11:30:40 -0300

    Adição do cabeçalho do exercício ao script

--- M1S04 - Ex 5 - Trello/script.js
+++ M1S04 - Ex 5 - Trello/script.js
@@ -1,3 +1,5 @@
+// Utilize uma estrutura condicional (if,else) para verificar se um número é positivo, negativo ou zero. Exiba os valores no console.log().
+
 function verificaValor(num) {
     if(num > 0) {
         console.log(`O número ${num} é positivo!`);
