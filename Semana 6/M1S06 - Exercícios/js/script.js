4e53b389a6c02e1ff1d5a5ec4844dbe926800c46
Thalles Azevedo <thalleskaian12@hotmail.com>
2024-02-27 09:41:39 -0300

    add mensagem de boas vindas

--- Semana 6/M1S06 - Exercícios/js/script.js
+++ Semana 6/M1S06 - Exercícios/js/script.js
@@ -3,6 +3,8 @@ const cards = document.querySelectorAll('.user-data')
 const button = document.querySelector('#btn')
 const body = document.querySelector('body')
 
+alert("Seja bem-vindo(a)")
+
 function diaNoite() {
     button.classList.toggle("ativo")
     body.classList.toggle("ativo")
